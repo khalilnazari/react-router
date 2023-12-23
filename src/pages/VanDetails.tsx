a93f12bdf5b6d2da9aee7ec8caffff483c3d0650
khalilnazari <khalil.deve@gmail.com>
2023-12-23 15:22:43 +0800

    Data fetch from mirage server

--- src/pages/VanDetails.tsx
+++ src/pages/VanDetails.tsx
@@ -1,11 +1,60 @@
+import { useParams } from "react-router-dom";
 import Container from "../components/Container";
 import PageTitle from "../components/PageTitle";
+import { useEffect, useState } from "react";
+
+type VanType = {
+  description: string | undefined;
+  id: string | undefined;
+  imageUrl: string | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  type: string | undefined;
+};
 
 const VanDetails = () => {
+  const [van, setVan] = useState<VanType>({
+    description: undefined,
+    id: undefined,
+    imageUrl: undefined,
+    name: undefined,
+    price: undefined,
+    type: undefined,
+  });
+
+  const params = useParams();
+
+  const fetchVans = async () => {
+    try {
+      const response = await fetch("/api/vans");
+      const data = await response.json();
+      const van = data.vans.find((van: VanType) => van.id === params.vanId);
+      setVan(van);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchVans();
+  }, []);
+
   return (
     <main>
       <Container>
-        <PageTitle>VanDetails</PageTitle>
+        <PageTitle>{van.name}</PageTitle>
+        <div className="flex gap-5">
+          <img
+            src={van?.imageUrl}
+            alt={van?.name}
+            className="max-w-[400px] rounded"
+          />
+          <div className="p-4 ">
+            <p className="text-xl mb-2">{van?.description}</p>
+            <p className="font-bold text-2xl mb-1">${van.price}</p>
+            <p className="font-bold text-2xl mb-1">{van.type}</p>
+          </div>
+        </div>
       </Container>
     </main>
   );
--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react";
 import Container from "../components/Container";
 import PageTitle from "../components/PageTitle";
+import { Link } from "react-router-dom";
+
+type VansType = {
+  description: string;
+  id: string;
+  imageUrl: string;
+  name: string;
+  price: number;
+  type: string;
+}[];
 
 const Vans = () => {
+  const [vans, setVans] = useState<VansType | []>([]);
+
+  const fetchVans = async () => {
+    try {
+      const response = await fetch("/api/vans");
+      const data = await response.json();
+      setVans(data.vans);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchVans();
+  }, []);
+
   return (
     <main>
       <Container>
         <PageTitle>Vans list</PageTitle>
+
+        <section className="grid grid-cols-3 gap-4">
+          {vans.length ? (
+            vans.map((van) => (
+              <Link to={`/vans/${van?.id}`} key={van.id} className="mb-2">
+                <img src={van.imageUrl} className="w-full rounded border" />
+                <div className="mt-2 flex justify-between p-1">
+                  <span className="font-bold">{van.name}</span>
+                  <span>{van.price}</span>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <div>No data found</div>
+          )}
+        </section>
       </Container>
     </main>
   );
