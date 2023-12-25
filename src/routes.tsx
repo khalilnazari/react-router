5d4892700205b26910bca5e7193f978b0ba2b508
khalilnazari <khalil.deve@gmail.com>
2023-12-25 14:51:49 +0800

    Converted routes to object format

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,4 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import PageLayout from "./components/PageLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -21,32 +17,108 @@ import HostVanDetails from "./pages/hosts/VanDetails";
 import Error from "./components/Error";
 import Login from "./pages/Login";
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<PageLayout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route
-        path="vans"
-        loader={vansLoader}
-        element={<Vans />}
-        errorElement={<Error />}
-      />
-      <Route path="vans/:vanId" element={<VanDetails />} />
-      <Route path="login" element={<Login />} />
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PageLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "vans",
+        element: <Vans />,
+        loader: vansLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "vans/:vanId",
+        element: <VanDetails />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "host",
+        element: <HostLayout />,
+        children: [
+          {
+            index: true,
+            element: <Dashboard />,
+          },
+          {
+            path: "reviews",
+            element: <Reviews />,
+          },
+          {
+            path: "income",
+            element: <Income />,
+          },
+          {
+            path: "vans",
+            element: <HostVans />,
+          },
 
-      <Route path="host" element={<HostLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="reviews" element={<Reviews />} />
-        <Route path="income" element={<Income />} />
-        <Route path="vans" element={<HostVans />} />
-        <Route path="vans/:vanId" element={<HostVan />}>
-          <Route index element={<HostVanDetails />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="photos" element={<Photos />} />
-        </Route>
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+          {
+            path: "vans/:vanId",
+            element: <HostVan />,
+            children: [
+              {
+                index: true,
+                element: <HostVanDetails />,
+              },
+              {
+                path: "pricing",
+                element: <Pricing />,
+              },
+              {
+                path: "photos",
+                element: <Photos />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+// export const routers = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<PageLayout />}>
+//       <Route index element={<Home />} />
+//       <Route path="about" element={<About />} />
+//       <Route
+//         path="vans"
+//         loader={vansLoader}
+//         element={<Vans />}
+//         errorElement={<Error />}
+//       />
+//       <Route path="vans/:vanId" element={<VanDetails />} />
+//       <Route path="login" element={<Login />} />
+
+//       <Route path="host" element={<HostLayout />}>
+//         <Route index element={<Dashboard />} />
+//         <Route path="reviews" element={<Reviews />} />
+//         <Route path="income" element={<Income />} />
+//         <Route path="vans" element={<HostVans />} />
+//         <Route path="vans/:vanId" element={<HostVan />}>
+//           <Route index element={<HostVanDetails />} />
+//           <Route path="pricing" element={<Pricing />} />
+//           <Route path="photos" element={<Photos />} />
+//         </Route>
+//       </Route>
+//       <Route path="*" element={<NotFound />} />
+//     </Route>
+//   )
+// );
